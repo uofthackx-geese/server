7f1d462eb0b7e70c4d8f04e1f40d09bd47e07481
Abhinav Gupta <abhinav.gupta@openlane.com>
2023-01-21 01:44:28 -0500

    Connecting to postgres

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
 from flask import Flask
 
+CREATE_TRAVEL_PLAN_TABLE = (
+    "CREATE TABLE IF NOT EXISTS travel_plan (id INT PRIMARY KEY AUTO_INCREMENT, title TEXT, type TEXT, description TEXT)"
+)
+
+#ADD_TRAVEL_DEST = INSERT 
+
+load_dotenv( )
+
 app = Flask(__name__)
-@app.route('/')
-def index():
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+@app.get('/')
+def home():
     return "GEEEEEESSEEE HUH"
 
 if __name__=="__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
