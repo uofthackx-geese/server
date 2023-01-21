13a1de49c53b7c67d8b6a9926e5bf7460a56c8a6
Abhinav Gupta <abhinav.gupta@openlane.com>
2023-01-21 03:40:17 -0500

    setup the flask and postgres conection as well 3 api calls

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
+
+# SQL QUERIES
 
 CREATE_TRAVEL_PLAN_TABLE = (
-    "CREATE TABLE IF NOT EXISTS travel_plan (id INT PRIMARY KEY AUTO_INCREMENT, title TEXT, type TEXT, description TEXT)"
+    "CREATE TABLE IF NOT EXISTS travel_plan (id SERIAL PRIMARY KEY, title TEXT, type TEXT, description TEXT);"
 )
 
-#ADD_TRAVEL_DEST = INSERT 
+ADD_TRAVEL_DEST = "INSERT INTO travel_plan (title, type, description) VALUES (%s, %s, %s) RETURNING id;"
+
+SELECT_ALL = "SELECT * FROM travel_plan;"
+
+DELETE_ALL = "DELETE FROM travel_plan;"
+
+RESTART_ID = "ALTER SEQUENCE travel_plan_id_seq RESTART WITH 1"
 
 load_dotenv( )
 
@@ -17,7 +25,36 @@ connection = psycopg2.connect(url)
 
 @app.get('/')
 def home():
-    return "GEEEEEESSEEE HUH"
+    return "COOL GEESE BACKEND :)"
+
+@app.post('/api/add_destination')
+def add_destination():
+    data = request.get_json()
+    title = data["title"]
+    type = data["type"]
+    desc = data["description"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_TRAVEL_PLAN_TABLE)
+            cursor.execute(ADD_TRAVEL_DEST, (title, type, desc))
+            dest_id = cursor.fetchone()[0]
+        return {"type": "SUCCESS", "id": dest_id, "message": "Destination {title} added."}, 201
+
+@app.post('/api/get_all')
+def get_all():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_ALL)
+            result = cursor.fetchall()
+            return {"type": "SUCCESS", "response": result}
+
+@app.post('/api/delete_all')
+def delete_all():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(DELETE_ALL)
+            cursor.execute(RESTART_ID)
+        return {"type": "SUCCESS"}, 201
 
 if __name__=="__main__":
     app.run(debug=True)
